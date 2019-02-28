c517728c12aa83924ce7a57686e29fe67fdd227b
YourFantasy <CZ950601@gmail.com>
2019-02-28 22:04:01 +0800

    添加了一些新的代码及更新readme

--- src/Two_Pointers/_713.java
+++ src/Two_Pointers/_713.java
@@ -5,6 +5,7 @@ public class _713 {
      * j为工作指针
      * i始终指向当b<k时的起始位置，并且此时i在j处或者j前面
      * 设置b为数组的连续的乘积，如果b>=k,此时不满足，所以b=b/nums[i];
+     * 设数组为a0,a1,a2,a3....ak,...am...an;如果ak*ak+1*...am<k,此时的个数为cnt，若ak*ak+1*...am*am+1<k, 此时的个数为cnt+m+1-k+1
      */
     public int numSubarrayProductLessThanK(int[] nums, int k) {
         int cnt = 0;
