2d22a9dd9ae02582edd9fe8b9a43307271a31b53
YourFantasy <CZ950601@gmail.com>
2019-02-28 21:45:34 +0800

    添加了一些新的代码

---
+++ src/Two_Pointers/_713.java
@@ -0,0 +1,24 @@
+package Two_Pointers;
+
+public class _713 {
+    /**
+     * j为工作指针
+     * i始终指向当b<k时的起始位置，并且此时i在j处或者j前面
+     * 设置b为数组的连续的乘积，如果b>=k,此时不满足，所以b=b/nums[i];
+     */
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        int cnt = 0;
+        int i = 0, j = 0;
+        int b = 1;
+        while (j < nums.length) {
+            b *= nums[j];
+            while (i <= j && b >= k) {
+                b = b / nums[i];
+                i++;
+            }
+            cnt += j - i + 1;//当（b<k）时的个数，如果b==1.此时表明单个元素的指都大于k，所以此时的个数为0；
+            j++;
+        }
+        return cnt;
+    }
+}
