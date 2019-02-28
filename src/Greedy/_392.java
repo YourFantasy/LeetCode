8e208cb3a1784d1d2137facd00e31162f2f9200b
YourFantasy <CZ950601@gmail.com>
2019-02-28 17:43:49 +0800

    添加了一些新的代码

---
+++ src/Greedy/_392.java
@@ -0,0 +1,17 @@
+package Greedy;
+
+public class _392 {
+    public boolean isSubsequence(String s, String t) {
+        int i = 0, j = 0;
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+                j++;
+            } else
+                j++;
+        }
+        if (i == s.length())
+            return true;
+        return false;
+    }
+}
