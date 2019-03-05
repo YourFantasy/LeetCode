39a5cf7db1eb4144666592dbb5bf7ab4b49e48ed
YourFantasy <CZ950601@gmail.com>
2019-03-05 22:07:13 +0800

    添加了一些代码

--- src/DFS/_39.java
+++ src/DFS/_39.java
@@ -6,12 +6,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class _39 {
-
+    //condidates中的元素不能为0，不然递归无法终止
     @Test
     public void Test() {
         int[] nums = new int[16];
         for (int i = 0; i < 16; i++) {
             nums[i] = (int) (Math.random() * 20);
+            if (nums[i] == 0)
+                nums[i] = i;
         }
 
         combinationSum(nums, 12);
@@ -29,17 +31,20 @@ public class _39 {
     }
 
     public void dfs(List<List<Integer>> result, List<Integer> temp, int target, int[] condidates, int start) {
+        if (target < 0)
+            return;
         if (target == 0) {
             if (!result.contains(temp))
                 result.add(new ArrayList<>(temp));
+            //result.add(temp);
             return;
         }
         for (int i = start; i < condidates.length; i++) {
-            if (target >= condidates[i]) {
-                temp.add(condidates[i]);
-                dfs(result, temp, target - condidates[i], condidates, i);
+
+            temp.add(condidates[i]);
+            dfs(result, temp, target - condidates[i], condidates, i);
+            if (temp.size() > 0)
                 temp.remove(temp.size() - 1);
-            }
         }
     }
 }
---
+++ src/DFS/_40.java
@@ -0,0 +1,45 @@
+package DFS;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class _40 {
+    @Test
+    public void test() {
+        int[] nums = {2, 5, 2, 1, 2, 3, 4};
+        int target = 5;
+        System.out.print(combinationSum2(nums, target));
+    }
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>> res = new ArrayList<>();
+        ArrayList<Integer> temp = new ArrayList<>();
+        dfs(candidates, res, temp, 0, target);
+        return res;
+    }
+
+    public void dfs(int[] condidates, List<List<Integer>> res, ArrayList<Integer> temp, int start, int target) {
+        if (target < 0)
+            return;
+        if (target == 0) {
+            if (!res.contains(temp)) {
+                System.out.println(temp);
+                res.add(new ArrayList<>(temp));
+                // res.add(temp);
+            }
+            return;
+        }
+        for (int i = start; i < condidates.length; i++) {
+            if (i > start && condidates[i] == condidates[i - 1])
+                continue;
+            temp.add(condidates[i]);
+            dfs(condidates, res, temp, i + 1, target - condidates[i]);
+            if (temp.size() > 0)
+                temp.remove(temp.size() - 1);
+        }
+    }
+}
