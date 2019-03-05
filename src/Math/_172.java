fb4b35476983146daaf436374bde9db7e776d8f4
YourFantasy <CZ950601@gmail.com>
2019-03-05 19:36:44 +0800

    添加了一些代码

---
+++ src/Math/_172.java
@@ -0,0 +1,12 @@
+package Math;
+
+public class _172 {
+    public int trailingZeroes(int n) {
+        int res = 0;
+        while (n >= 5) {
+            n /= 5;
+            res += n;
+        }
+        return res;
+    }
+}
