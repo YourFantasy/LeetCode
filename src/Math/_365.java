f01c7fccc81030fc4e9fb22d83030636e3c834c3
YourFantasy <CZ950601@gmail.com>
2019-03-08 21:01:00 +0800

    添加了一些代码

---
+++ src/Math/_365.java
@@ -0,0 +1,28 @@
+package Math;
+
+public class _365 {
+    public boolean canMeasureWater(int x, int y, int z) {
+        if (x + y < z)
+            return false;
+        if (x + y + z == 0)
+            return true;
+        int m = get_divisor(x, y);
+        if (z % m == 0)
+            return true;
+        return false;
+    }
+
+    int get_divisor(int m, int n) {
+        if (m < n) {
+            int temp = m;
+            m = n;
+            n = temp;
+        }
+        while (n > 0) {
+            int by = m % n;
+            m = n;
+            n = by;
+        }
+        return m;
+    }
+}
