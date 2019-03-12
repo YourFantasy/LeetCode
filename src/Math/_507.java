55283e2b75628ee9dda55de1e5733151bfcd5197
YourFantasy <CZ950601@gmail.com>
2019-03-12 10:46:13 +0800

    readme

---
+++ src/Math/_507.java
@@ -0,0 +1,13 @@
+package Math;
+
+public class _507 {
+    public boolean checkPerfectNumber(int num) {
+        if (num < 2)
+            return false;
+        int sum = 1;
+        for (int i = 2; i * i <= num; i++)
+            if (num % i == 0)
+                sum += (i + num / i);
+        return sum == num;
+    }
+}
