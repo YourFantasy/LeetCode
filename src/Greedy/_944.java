fa576273fe50a82105f82f6be572677ef2b8adf8
YourFantasy <CZ950601@gmail.com>
2019-03-17 21:18:55 +0800

    添加了一些新的代码

---
+++ src/Greedy/_944.java
@@ -0,0 +1,28 @@
+package Greedy;
+
+import org.junit.Test;
+
+public class _944 {
+    @Test
+    public void test() {
+        String[] A = {"cba", "aaf", "ghi"};
+        System.out.print(minDeletionSize(A));
+    }
+
+    public int minDeletionSize(String[] A) {
+        int cnt = 0;
+        for (int i = 0; i < A[0].length(); i++) {
+            if (!is_Ascend(A, i))
+                cnt++;
+        }
+        return cnt;
+    }
+
+    boolean is_Ascend(String[] A, int column) {
+        for (int i = 1; i < A.length; i++) {
+            if (A[i].charAt(column) - A[i - 1].charAt(column) < 0)
+                return false;
+        }
+        return true;
+    }
+}
