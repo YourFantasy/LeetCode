4c10dde650842b68d8b03c0b8a8f6b8c991a49bf
YourFantasy <CZ950601@gmail.com>
2019-04-03 21:03:38 +0800

    更新一些新的代码

--- src/Binary_Search/_35.java
+++ src/Binary_Search/_35.java
@@ -2,17 +2,15 @@ package Binary_Search;
 
 public class _35 {
     public int searchInsert(int[] nums, int target) {
-        int i=0,j=nums.length-1;
-        while(i<=j){
-            int mid=(i+j)/2;
-            if(nums[mid]==target){
+        int i = 0, j = nums.length - 1;
+        while (i <= j) {
+            int mid = (i + j) / 2;
+            if (nums[mid] == target) {
                 return mid;
-            }
-            else if(nums[mid]<target){
-                i=mid+1;
-            }
-            else
-                j=mid-1;
+            } else if (nums[mid] < target) {
+                i = mid + 1;
+            } else
+                j = mid - 1;
         }
         return i;
     }
--- src/DP/_120.java
+++ src/DP/_120.java
@@ -3,17 +3,22 @@ package DP;
 import java.util.List;
 
 public class _120 {
+    /**
+     * 从上往下走与从下往下走是等价的。
+     * @param triangle
+     * @return
+     */
     public int minimumTotal(List<List<Integer>> triangle) {
-        int len = triangle.size();
-        int[] dp = new int[len];
-        dp[0] = triangle.get(0).get(0);
-        for (int i = 0; i < len-1; i++) {
-            List<Integer> list = triangle.get(i);
-            for (int j = 0; j < list.size(); j++) {
+        int n = triangle.size();
+        if (n == 0) {
+            return 0;
+        }
+        int[] dp = new int[n + 1];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[j] = triangle.get(i).get(j) + Math.min(dp[j], dp[j + 1]);
             }
         }
-        return dp[len-1];
+        return dp[0];
     }
-
-
 }
