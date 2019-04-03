ba983439a9c0742ecb780954f27621fc9514b22e
YourFantasy <CZ950601@gmail.com>
2019-04-03 22:03:25 +0800

    更新一些新的代码

---
+++ src/Math/_12.java
@@ -0,0 +1,31 @@
+package Math;
+
+import org.junit.Test;
+
+public class _12 {
+    @Test
+    public void test() {
+        System.out.println(intToRoman(159));
+    }
+
+    public String intToRoman(int num) {
+        int[] digits = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+        String[] roman = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+        String res = "";
+//        for (int i=roman.length-1;i>=0;i--){
+////            while (num>=digits[i]){
+////                num-=digits[i];
+////                res+=roman[i];
+////            }
+////        }
+        int i=digits.length-1;
+        while (num>0){
+            int n=num/digits[i];
+            for (int j=0;j<n;j++)
+                res+=roman[i];
+            num=num%digits[i];
+            i--;
+        }
+        return res;
+    }
+}
