3bdc2666e1fd2aa2a3c431369e9fa685d9cf8926
YourFantasy <CZ950601@gmail.com>
2019-04-10 22:10:12 +0800

    更新了一些新的代码

---
+++ src/String/_58.java
@@ -0,0 +1,23 @@
+package String;
+
+import org.junit.Test;
+
+public class _58 {
+    @Test
+    public void test() {
+        String s = "abc  ";
+        System.out.println(lengthOfLastWord(s));
+    }
+
+    public int lengthOfLastWord(String s) {
+        s = s.trim();
+        int cnt = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ')
+                cnt = 0;
+            else
+                cnt++;
+        }
+        return cnt;
+    }
+}
