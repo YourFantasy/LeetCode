66b473ed8fc648f5a48d6a61a902e5e67981d2e7
YourFantasy <CZ950601@gmail.com>
2019-04-10 22:28:55 +0800

    更新了一些新的代码

---
+++ src/Math/_69.java
@@ -0,0 +1,17 @@
+package Math;
+
+public class _69 {
+    public int mySqrt(int x) {
+        long i = 1, j = x;
+        while (i <= j) {
+            long mid = i + (j - i) / 2;
+            if (mid * mid <= x && (mid + 1) * (mid + 1) > x)
+                return (int) mid;
+            else if (mid * mid > x)
+                j = mid - 1;
+            else if (mid * mid < x)
+                i = mid + 1;
+        }
+        return 0;
+    }
+}
--- src/String/_58.java
+++ src/String/_58.java
@@ -4,13 +4,12 @@ import org.junit.Test;
 
 public class _58 {
     @Test
-    public void test() {
-        String s = "abc  ";
+    public void test(){
+        String s="abc  ";
         System.out.println(lengthOfLastWord(s));
     }
-
     public int lengthOfLastWord(String s) {
-        s = s.trim();
+        s=s.trim();
         int cnt = 0;
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == ' ')
