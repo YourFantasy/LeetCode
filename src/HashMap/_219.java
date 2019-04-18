f2430b0384e398bb12a0bee16de132f3fdecc6b5
YourFantasy <CZ950601@gmail.com>
2019-04-18 21:48:52 +0800

    添加了一些新的代码

---
+++ src/HashMap/_219.java
@@ -0,0 +1,16 @@
+package HashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class _219 {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])&&i!=map.get(nums[i])&&Math.abs(i-map.get(nums[i]))<=k)
+                return true;
+                map.put(nums[i], i);
+        }
+        return false;
+    }
+}
