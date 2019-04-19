82910c6ebb00384a619f0ce3af42bb4bf9a28a3e
YourFantasy <CZ950601@gmail.com>
2019-04-19 18:48:48 +0800

    更新readme

---
+++ src/Math/_991.java
@@ -0,0 +1,27 @@
+package Math;
+
+import org.junit.Test;
+
+public class _991 {
+    @Test
+    public void test() {
+        int x = 1, y = 1000;
+        System.out.println(brokenCalc(x, y));
+    }
+
+    public int brokenCalc(int X, int Y) {
+        int cnt = 0;
+        if (Y <= X)
+            return X - Y;
+        while (X < Y) {
+            if (Y % 2 == 0) {
+                Y /= 2;
+                cnt++;
+            } else {
+                Y = (Y + 1) / 2;
+                cnt += 2;
+            }
+        }
+        return X - Y + cnt;
+    }
+}
---
+++ src/Tree/_437.java
@@ -0,0 +1,27 @@
+package Tree;
+
+public class _437 {
+    private int cnt = 0;
+
+    public int pathSum(TreeNode root, int sum) {
+        preorder(root, sum);
+        return cnt;
+    }
+
+    public void preorder(TreeNode root, int sum) {
+        if (root == null)
+            return;
+        dfs(root, sum);
+        preorder(root.left, sum);
+        preorder(root.right, sum);
+    }
+
+    public void dfs(TreeNode root, int sum) {
+        if (root == null)
+            return;
+        if (root.val == sum)
+            cnt++;
+        dfs(root.left, sum - root.val);
+        dfs(root.right, sum - root.val);
+    }
+}
