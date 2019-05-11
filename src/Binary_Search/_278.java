48b36e9d14f0c86995b344b463268d0b6ae542ac
YourFantasy <cz950601@gmail.com>
2019-05-11 23:02:05 +0800

    添加了一些新的代码

---
+++ src/Binary_Search/_278.java
@@ -0,0 +1,19 @@
+package Binary_Search;
+
+public class _278 {
+    public int firstBadVersion(int n) {
+        int i = 1, j = n;
+        while (i < j) {
+            int mid = i + (j - i) / 2;
+            if (!isBadVersion(mid))
+                i = mid + 1;
+            else
+                j = mid;
+        }
+        return i;
+    }
+
+    boolean isBadVersion(int version) {
+        return true;
+    }
+}
