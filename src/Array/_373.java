100d307a074387ab16e9b811458edacdd6833d27
YourFantasy <cz950601@gmail.com>
2019-06-13 20:57:12 +0800

    添加了一些新的代码

---
+++ src/Array/_373.java
@@ -0,0 +1,49 @@
+package Array;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class _373 {
+    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        List<List<Integer>> res = new ArrayList<>();
+        int len1 = nums1.length, len2 = nums2.length;
+        if (len1 * len2 == 0)
+            return res;
+        k = Math.min(len1 * len2, k);
+        int[] temp = new int[len1];
+        for (int i = 0; i < k; i++) {
+            int min = Integer.MAX_VALUE;
+            int index = 0;
+            for (int j = 0; j < len1; j++) {
+                if (temp[j] < len2 && nums2[temp[j]] + nums1[j] < min) {
+                    min = nums2[temp[j]] + nums1[j];
+                    index = j;
+                }
+            }
+            List<Integer> list = new ArrayList<>();
+            list.add(nums1[index]);
+            list.add(nums2[temp[index]]);
+            res.add(list);
+            temp[index]++;
+        }
+        return res;
+    }
+
+    @Test
+    public void test() {
+        int len1 = (int) (Math.random() * 20);
+        int len2 = (int) (Math.random() * 20);
+        int[] nums1 = new int[len1];
+        int[] nums2 = new int[len2];
+        for (int i = 0; i < len1; i++)
+            nums1[i] = (int) (Math.random() * 20);
+        for (int i = 0; i < len2; i++)
+            nums2[i] = (int) (Math.random() * 20);
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        System.out.println(kSmallestPairs(nums1, nums2, (int) (Math.random() * 20)));
+    }
+}
--- src/DP/_10.java
+++ src/DP/_10.java
@@ -1,10 +1,13 @@
 package DP;
 
+/**
+ * dp[i][j]表示s[0-i]和p[0-j]是否能够匹配
+ */
 public class _10 {
 
     public boolean isMatch(String s, String p) {
         int len1 = s.length(), len2 = p.length();
-        boolean[][] dp = new boolean[len1 +][len2];
+        boolean[][] dp = new boolean[len1 +1][len2+1];
         dp[0][0] = true;
         for (int i = 1; i <= len2; i++) {
             //dp[0][i] = (p.charAt(i - 1) == '*') && (dp[0][i - 1] || (i > 1 && dp[0][i - 2]));
