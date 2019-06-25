c06a9074b309be4a201d4583acf11e8c06e9e378
YourFantasy <chen@Chen-MBP.local>
2019-06-25 14:55:49 +0800

    添加了一些新的代码

---
+++ src/Binary_Search/_81.java
@@ -0,0 +1,24 @@
+package Binary_Search;
+
+public class _81 {
+    public boolean search(int[] nums, int target) {
+        int i = 0, j = nums.length - 1;
+        while (i <= j) {
+            int mid = i + (j - i) / 2;
+            if (target == nums[mid])
+                return true;
+            if (nums[mid] > nums[i]) {
+                if (target >= nums[i] && target <= nums[mid])
+                    j = mid;
+                else
+                    i = mid + 1;
+            } else if (nums[mid] < nums[i]) {
+                if (target >= nums[mid] && target <= nums[j])
+                    i = mid;
+                else
+                    j = mid - 1;
+            } else i += 1;
+        }
+        return false;
+    }
+}
