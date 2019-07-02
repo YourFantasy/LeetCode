aded30ca47af0c17bb6e1f9dc8a00437fe4485bd
YourFantasy <chen@Chen-MBP.local>
2019-07-02 10:03:47 +0800

    添加了一些新的代码

--- src/Binary_Search/_153.java
+++ src/Binary_Search/_153.java
@@ -1,7 +1,7 @@
 package Binary_Search;
 
 public class _153 {
-    public int findMin(int[] nums) {
+    public int findMin1(int[] nums) {
         int i = 0, j = nums.length - 1;
         if (nums[j] > nums[i])
             return nums[i];
@@ -13,5 +13,18 @@ public class _153 {
                 j = mid;
         }
         return Math.min(nums[i], nums[j]);
+    }
+
+    public int findMin2(int[] nums) {
+        int i = 0, j = nums.length - 1;
+        while (i < j) {
+            int mid = i + ((j - i) >> 1);
+            if (nums[mid] > nums[j])
+                i = mid + 1;
+            else
+                j = mid;
+        }
+
+        return nums[i];
     }
 }
