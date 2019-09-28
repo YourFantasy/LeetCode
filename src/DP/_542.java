9898d828cb019dc2d48f2b873da7eed11a73cab2
YourFantasy <cz950601@gmail.com>
2019-09-28 09:47:08 +0800

    添加了一些新的代码

---
+++ src/DP/_542.java
@@ -0,0 +1,66 @@
+package DP;
+
+import org.junit.Test;
+
+/**
+ * @author YourFantasy
+ * @date 2019/9/26 1:40 下午
+ * @description: 01矩阵
+ */
+public class _542 {
+    @Test
+    public void test() {
+        int m = 10, n = 5;
+        int[][] matrix = new int[m][n];
+        int[] data = {0, 1};
+        int len = data.length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int index = (int) (Math.random() * len);
+                matrix[i][j] = data[index];
+            }
+        }
+        updateMatrix(matrix);
+    }
+
+    private int[][] updateMatrix(int[][] matrix) {
+        int row = matrix.length;
+        int column = matrix[0].length;
+        int[][] dp = new int[row][column];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                dp[i][j] = row * column;
+            }
+        }
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                if (matrix[i][j] == 0) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+                if (i > 0) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i - 1][j] + 1);
+                }
+                if (j > 0) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i][j - 1] + 1);
+                }
+            }
+        }
+        for (int i = row - 1; i >= 0; i--) {
+            for (int j = column - 1; j >= 0; j--) {
+                if (matrix[i][j] == 0) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+                if (i < row - 1) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i + 1][j] + 1);
+                }
+                if (j < column - 1) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i][j + 1] + 1);
+                }
+            }
+        }
+        return dp;
+    }
+
+}
---
+++ src/HashMap/_409.java
@@ -0,0 +1,41 @@
+package HashMap;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author YourFantasy
+ * @date 2019/9/22 12:19 下午
+ * @description: 最长回文串
+ */
+public class _409 {
+    @Test
+    public void test() {
+        String str = "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth";
+        int res = longestPalindrome(str);
+        System.out.println(res);
+
+    }
+
+    private int longestPalindrome(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (!map.containsKey(ch)) {
+                map.put(ch, 1);
+            } else {
+                map.put(ch, map.get(ch) + 1);
+            }
+        }
+        int cnt = 0;
+        for (Character key : map.keySet()) {
+            int val = map.get(key);
+            if (val % 2 == 1) {
+                cnt++;
+            }
+        }
+        return cnt == 0 ? s.length() : s.length() - cnt + 1;
+    }
+}
