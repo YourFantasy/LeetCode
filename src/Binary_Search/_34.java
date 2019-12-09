678008f1aa5d3cdd8277fa99ec9e3ff3309f6e50
YourFantasy <cz950601@gmail.com>
2019-12-09 12:32:37 +0800

    重构代码

--- src/Binary_Search/_34.java
+++ src/Binary_Search/_34.java
@@ -45,4 +45,37 @@ class _34 {
         return end;
     }
 
+    public int[] searchRange1(int[] nums, int target) {
+        int[] res = new int[2];
+        res[0] = -1;
+        res[1] = -1;
+        if (nums == null || nums.length == 0) {
+            return res;
+        }
+        int i = 0, j = nums.length - 1;
+        while (i < j) {
+            int m = i + (j - i) / 2;
+            if (nums[m] < target) {
+                i = m + 1;
+            } else {
+                j = m;
+            }
+        }
+
+        if (nums[i] != target) {
+            return res;
+        }
+        res[0] = i;
+        j = nums.length - 1;
+        while (i <= j) {
+            int m = i + (j - i) / 2;
+            if (nums[m] > target) {
+                j = m - 1;
+            } else {
+                i = m + 1;
+            }
+        }
+        res[1] = j;
+        return res;
+    }
 }
