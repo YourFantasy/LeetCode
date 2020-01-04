6b3a283051999847e641a1e3c5158503f041124e
YourFantasy <cz950601@gmail.com>
2020-01-04 11:57:51 +0800

    添加了新的代码

---
+++ src/DP/_650.java
@@ -0,0 +1,26 @@
+package DP;
+
+public class _650 {
+    public int minSteps(int n) {
+        if (n == 1) {
+            return 0;
+        }
+        int[] dp = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[i] = i;
+        }
+        for (int i = 2; i <= n; i++) {
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    int k = i / j;
+                    if (k >= 2) {
+                        dp[i] = Math.min(dp[i], Math.min(k + dp[j], j + dp[k]));
+                    } else {
+                        dp[i] = Math.min(dp[i], Math.min(2 * k - 1 + dp[j], j + dp[k]));
+                    }
+                }
+            }
+        }
+        return dp[n];
+    }
+}
