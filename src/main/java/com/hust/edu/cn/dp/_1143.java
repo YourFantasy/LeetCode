fc860f2250a03ccd857c249bb76b12e2fb7bd63e
Chen Zhi <chenzhibj@fenbi.com>
2020-01-26 21:08:40 +0800

    添加了一些新的代码

---
+++ src/main/java/com/hust/edu/cn/dp/_1143.java
@@ -0,0 +1,30 @@
+/**
+ * @(#)_1143.java, 1月 26, 2020.
+ * <p>
+ * Copyright 2020 fenbi.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.hust.edu.cn.dp;
+
+/**
+ * @author chenzhibj
+ */
+public class _1143 {
+
+    public int longestCommonSubsequence(String text1, String text2) {
+        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
+        for (int i = 1; i <= text1.length(); i++) {
+            for (int j = 1; j <= text2.length(); j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+
+        return dp[text1.length()][text2.length()];
+
+    }
+}
