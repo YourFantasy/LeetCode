e34fc05b43738071ad1b98e5bf5699039605ce89
ChenZhi <cz950601@gmail.com>
2020-07-20 16:37:06 +0800

    UPDATE[25:K 个一组翻转链表]，添加新的解法

--- src/main/java/com/hust/edu/cn/linkedList/_25.java
+++ src/main/java/com/hust/edu/cn/linkedList/_25.java
@@ -1,7 +1,7 @@
 package com.hust.edu.cn.linkedList;
 
 public class _25 {
-    public ListNode reverseKGroup(ListNode head, int k) {
+    public ListNode reverseKGroup1(ListNode head, int k) {
         int n = k;
         ListNode p = head, end = null;
         while (p != null && n != 0) {
@@ -12,7 +12,7 @@ public class _25 {
         if (n != 0) {
             return head;
         }
-        end.next = reverseKGroup(end.next, k);
+        end.next = reverseKGroup1(end.next, k);
         while (head != end) {
             p = head;
             head = head.next;
@@ -21,4 +21,58 @@ public class _25 {
         }
         return end;
     }
+
+    public ListNode reverseKGroup2(ListNode head, int k) {
+        if (head == null || k < 2) {
+            return head;
+        }
+        int cnt = 0;
+        ListNode p = head, pre = null;
+        while (p != null && cnt < k) {
+            cnt++;
+            pre = p;
+            p = p.next;
+        }
+        if (cnt != k) {
+            return head;
+        }
+        ListNode q = head;
+        while (q != pre) {
+            ListNode next = q.next;
+            q.next = pre.next;
+            pre.next = q;
+            q = next;
+        }
+
+        head.next = reverseKGroup2(p, k);
+        return pre;
+    }
+
+    public ListNode reverseKGroup3(ListNode head, int k) {
+        if (head == null || k < 2) {
+            return head;
+        }
+        int len = 0;
+        ListNode res = new ListNode(-1);
+        res.next = head;
+        ListNode pre = res, p = pre.next;
+        while (p != null) {
+            p = p.next;
+            len++;
+        }
+        p = pre.next;
+        for (int i = 0; i < len / k; i++) {
+            ListNode next;
+            for (int j = 0; j < k - 1; j++) {
+                next = p.next;
+                p.next = next.next;
+                next.next = pre.next;
+                pre.next = next;
+            }
+            pre = p;
+            p = pre.next;
+        }
+        return res.next;
+    }
+
 }
