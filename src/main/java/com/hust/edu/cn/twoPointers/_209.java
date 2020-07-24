66ce970b4f3501da50d1d6aec7ac31972f108f04
ChenZhi <cz950601@gmail.com>
2020-07-24 16:43:13 +0800

    UPDATE:[209:长度最小的子数组],添加新的解法

--- src/main/java/com/hust/edu/cn/twoPointers/_209.java
+++ src/main/java/com/hust/edu/cn/twoPointers/_209.java
@@ -2,7 +2,7 @@ package com.hust.edu.cn.twoPointers;
 
 class _209 {
     //使用双指针实现滑动窗口，i始终指向满足sum>=s时左边元素，j是工作指针。i作为指向左边元素的指针，所以不能指向最后一个元素，不然sum为0。
-    public int minSubArrayLen(int s, int[] nums) {
+    public int minSubArrayLen1(int s, int[] nums) {
         int res = nums.length + 1;
         int i = 0, j = 0;
         int sum = 0;
@@ -23,5 +23,42 @@ class _209 {
             return res;
         }
         return 0;
+    }
+
+    /**
+     * 使用前缀和
+     *
+     * @param s
+     * @param nums
+     * @return
+     */
+    public int minSubArrayLen2(int s, int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i - 1];
+        }
+        if (nums[nums.length - 1] < s) {
+            return 0;
+        }
+        int minLen = nums.length + 1;
+        int i = -1, j = 0;
+        while (i < j && j < nums.length) {
+            int sub;
+            if (i < 0) {
+                sub = nums[j];
+            } else {
+                sub = nums[j] - nums[i];
+            }
+            if (sub >= s) {
+                minLen = Math.min(minLen, j - i);
+                i++;
+            } else {
+                j++;
+            }
+        }
+
+        return minLen == nums.length + 1 ? 0 : minLen;
     }
 }
