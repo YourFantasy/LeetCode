8ad4de8611edc2bc78ee80c59c5e2a06cf631508
ChenZhi <cz950601@gmail.com>
2020-08-25 22:15:18 +0800

    UPDATE:[643:子数组最大平均数 I,添加了新的解法]

--- src/main/java/com/hust/edu/cn/array/_643.java
+++ src/main/java/com/hust/edu/cn/array/_643.java
@@ -3,20 +3,58 @@ package com.hust.edu.cn.array;
 import org.junit.Test;
 
 public class _643 {
-@Test
-    public void Test(){
-    System.out.println(Double.MIN_VALUE);
-}
+    @Test
+    public void Test() {
+        int n = 10, k = 4;
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = (int) (Math.random() * 4 * n - 3 * n);
+        }
+        System.out.println(findMaxAverage1(nums, k));
+        System.out.println(findMaxAverage2(nums, k));
+    }
 
-    public double findMaxAverage(int[] nums, int k) {
-        double result=Double.MIN_VALUE;
-        for(int i=0;i<=nums.length-k;i++){
-            double sum=0;
-            for(int j=i;j<i+k;j++){
-                sum+=nums[j];
+    /**
+     * 暴力解法，超时
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public double findMaxAverage1(int[] nums, int k) {
+        double result = Double.MIN_VALUE;
+        for (int i = 0; i <= nums.length - k; i++) {
+            int sum = 0;
+            for (int j = i; j < i + k; j++) {
+                sum += nums[j];
             }
-            result=Math.max(result,sum/k);
+            result = Math.max(result, (double) sum / k);
         }
         return result;
+    }
+
+    /**
+     * 前缀和
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public double findMaxAverage2(int[] nums, int k) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i - 1];
+        }
+        int sum, max = Integer.MIN_VALUE;
+        for (int i = k - 1; i < nums.length; i++) {
+            if (i == k - 1) {
+                sum = nums[i];
+            } else {
+                sum = nums[i] - nums[i - k];
+            }
+            max = Math.max(max, sum);
+        }
+
+        return (double) max / k;
+
     }
 }
